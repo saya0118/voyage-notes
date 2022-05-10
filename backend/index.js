8e929647a44d82c8594605bbfbfc3d487c634405
saya0118 <chemi0802@gmail.com>
2022-05-09 20:12:37 -0700

    installed mongoose express dotenv

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+app.use(express.json());
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB is connected!");
+  })
+  .catch((err) => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
