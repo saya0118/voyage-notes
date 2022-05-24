02c9a1acc705ed70e7befb854d4bc4e4600c87df
saya0118 <chemi0802@gmail.com>
2022-05-23 21:49:50 -0700

    installed react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, Router } from "react";
 import MainPage from "./components/MainPage/MainPage";
 import Header from "./components/Header/Header";
 
