653dff87f0c39b50f0b3c818d3c7dfbda7c16155
saya0118 <chemi0802@gmail.com>
2022-05-28 22:57:01 -0700

    added edit action

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,8 +1,16 @@
 export const LIST_ADDED = "LIST_ADDED";
+export const LIST_EDITED = "LIST_EDITED";
 
 export const Add = (country) => {
     return {
         type: "LIST_ADDED",
+        payload: country,
+    }
+}
+
+export const Edit = (country) => {
+    return {
+        type: "LIST_EDITED",
         payload: country,
     }
 }
